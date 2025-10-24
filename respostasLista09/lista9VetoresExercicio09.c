7780864ce3e2ab25cc72cc667caa22c00535f522
leopellizzoni <lpellizzoni@ucs.br>
2025-10-24 20:47:58 -0300

    respostas da lista 9 de exercícios sobre vetores

---
+++ respostasLista09/lista9VetoresExercicio09.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int valores[50];
+    int contador,  maior = 0;
+
+    for (contador = 0; contador <= 49; contador++)
+    {
+        printf("Informe o valor para a posicao %d: ", contador);
+        scanf("%d", &valores[contador]);
+    }
+
+    for (contador = 0; contador <= 49; contador++)
+    {
+        if (valores[contador] > maior)
+        {
+            maior = valores[contador];
+        }
+    }
+
+    printf("maior numero: %d", maior);
+
+    return 0;
+}
---
+++ respostasLista09/lista9VetoresExercicio12.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    float valores[15];
+    int contador,temValor = 0;
+    float media, menorAcimaDaMedia = 0;
+
+    for (contador = 0; contador <= 14; contador++)
+    {
+        printf("Informe o valor para a posicao %d: ", contador);
+        scanf("%f", &valores[contador]);
+        media = media + valores[contador];
+    }
+
+    media = media / 15;
+
+    for (contador = 0; contador <= 9; contador++)
+    {
+        if (valores[contador] > media)
+        {
+            // Não sabemos qual vai ser a posicao do primeiro numero maior que a media
+            // Por isso um controle e no primeiro maior que a media ja coloca definido
+            if (temValor == 0)
+            {
+                temValor = 1;
+                menorAcimaDaMedia = valores[contador];
+            }
+            else if (valores[contador] < menorAcimaDaMedia)
+            {
+                menorAcimaDaMedia = valores[contador];
+            }
+        }
+    }
+
+    printf("\n\nMedia foi de %f\n", media);
+
+    if (temValor)
+    {
+        printf("O menor acima da media foi: %f", menorAcimaDaMedia);
+    }
+    else
+    {
+        printf("Nao foi encontrado o menor valor maior que a media");
+    }
+
+    return 0;
+}
